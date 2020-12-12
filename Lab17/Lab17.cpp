6c9b2fb0fa940dd6f30f2cd9736cfe010c5f3b56
Draiza Koceshat <enderdraizaking@gmail.com>
2020-12-12 17:42:54 +0300

    Добавьте файлы проекта.

---
+++ Lab17/Lab17.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.h>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    wcout.imbue(locale(".1251"));
+    wcin.imbue(locale(".866"));
+    int n, k, i;
+
+    //1
+    cin >> n;
+    f = 1;
+    int* a = new int[n];
+    for (i = 0; i < n; i++)
+    {
+        a[i] = f;
+        cout << a[i] << endl;
+        f = f + 2;
+    }
+}
